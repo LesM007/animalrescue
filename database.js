a703e7bb630ada3a42107ebcd536ca95de02e594
LesM007 <U1501857@RTS-365.dk>
2023-03-30 12:39:26 +0200

    MONGO_PW update commit

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const { connect } = require("mongoose");
 
-const connectionString = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_DB}@${process.env.MONGO_SERVER}/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`;
+const connectionString = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@${process.env.MONGO_SERVER}/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`;
 
 connect(connectionString, {
   useNewUrlParser: true,
